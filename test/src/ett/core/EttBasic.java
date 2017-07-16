8a76c5b18e1dba49a46055712a84fb025db81d68
lifedomy <lifedomy@gmail.com>
2017-07-17 02:11:58 +0900

    aa

--- test/src/ett/core/EttBasic.java
+++ test/src/ett/core/EttBasic.java
@@ -33,7 +33,7 @@ public class EttBasic implements Ett {
 
 	List<TableSchemaVo> tableSchemaVoList = new ArrayList<TableSchemaVo>();
 
-    // main        --22
+    // main        --2233
 	public static void main(String[] args) {
 		EttBasic ettBasic = new EttBasic();
 		
